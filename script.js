f7970357ad0a283693857902e6a02817169ab45a
Viniciusulpicio <viniciusulpicio@gmail.com>
2024-06-15 21:21:28 -0300

    upload da home, pag serviços, produtos e depoimentos.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Essa função carrega outra pagina no index, como se fossem components
+function loadContent(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadContent('./pages/servicos.html', 'servicos');
+    loadContent('./pages/produtos.html', 'produtos');
+    loadContent('./pages/depoimentos.html', 'depoimentos');
+});
